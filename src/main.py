cad39f4c4a6c5973dc52feb89257c11f62115ba4
lalobgc <lalobg.c@gmail.com>
2025-03-04 12:44:35 -0600

    rebuild

--- src/main.py
+++ src/main.py
@@ -17,15 +17,16 @@ def main():
     #node = TextNode("Test node", TextType.TEXT)
     #print(node)
     static_to_docs()
-    md_path = "/Users/lalobec/workspace/github.com/lalokure/static_site_gen/content"
-    template_path ="/Users/lalobec/workspace/github.com/lalokure/static_site_gen/template.html"
-    dest_path = "/Users/lalobec/workspace/github.com/lalokure/static_site_gen/docs"
+    md_path = os.path.join(base_dir, "..", "content")
+    template_path = os.path.join(base_dir, "..", "template.html")
+    dest_path = os.path.join(base_dir, "..", "docs") 
     generate_pages_recursive(md_path, template_path, dest_path, basepath)
 
 
 def static_to_docs():
-    static_path = "/Users/lalobec/workspace/github.com/lalokure/static_site_gen/static"
-    public_path = "/Users/lalobec/workspace/github.com/lalokure/static_site_gen/docs"
+    static_path = os.path.join(base_dir, "..", "static") 
+    public_path = os.path.join(base_dir, "..", "docs")
+    print(f"Copying from {static_path} to {public_path}")
     shutil.rmtree(public_path, ignore_errors = True)
     os.mkdir(public_path)
     copy_files_and_directories(static_path, public_path)
